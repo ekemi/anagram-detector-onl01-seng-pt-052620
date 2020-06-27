b1364e5c63532d0d8c013260edddf20c17adff72
edmundo papi <edmundopierre@yahoo.com>
2020-06-27 14:56:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+require "pry"
+class Anagram
+   attr_accessor :word
+  def initialize(word)
+
+    @word = word
+  end
+
+  def match(array_words)
+
+    array =%w()
+    array_words.each{
+      |value|
+      if value != @word
+            return array
+
+      elsif value == @word.reverse
+            array << @word.reverse
+            return array
+
+      end
+
+    }
+
+  end
+end
